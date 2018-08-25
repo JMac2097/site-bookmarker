317ec1e8f6b3eaa998db6c6c5846cace9ec04b58
John McKie <therealjmac2097@gmail.com>
2018-08-25 17:58:04 +0100

    initial commit -- setup of project and initial js

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+// Listen for page load
+window.addEventListener('load', fetchBookmarks);
+
+// listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+//save bookmark
+function saveBookmark(e) {
+
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    // console.log(siteName);
+    // console.log(siteUrl);
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    };
+
+    // localStorage.setItem('test', 'hello world');
+    // console.log(localStorage.getItem('test'));
+    // localStorage.removeItem('test');
+    // console.log(localStorage.getItem('test'));
+
+    // test if bookmarks exist
+    if(localStorage.getItem('bookmarks') === null) {
+        // init array
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        // set to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // get bookmarks from localstorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // add bookmark to array
+        bookmarks.push(bookmark);
+        // reset back to localstorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    };
+
+    // prevent form submission
+    e.preventDefault();
+};
+
+function fetchBookmarks() {
+    // get bookmarks from localstorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    console.log('loaded');
+}
